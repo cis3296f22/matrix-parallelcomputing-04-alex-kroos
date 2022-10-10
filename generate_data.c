24193e79fbe2460b559f3349ff33de39b2d147aa
mailoalex <tun29878@temple.edu>
2022-10-10 13:47:46 -0400

    added basic program and organized files

---
+++ generate_data.c
@@ -0,0 +1,48 @@
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "mat.h"
+#include <sys/time.h>
+int generate_data(int matrix_size, char *output_filename)
+{
+    FILE *fp;
+    fp = fopen(output_filename, "a");
+
+    struct timeval stop, start;
+    int rows, cols;
+
+    rows = matrix_size;
+    cols = matrix_size;
+    double *a, *b, *result;
+    
+    result = malloc(rows * cols * sizeof(double));
+    a = gen_matrix(rows, cols);
+    b = gen_matrix(rows, cols);
+    
+    gettimeofday(&start, NULL);
+    mmult(result, a, rows, cols, b, rows, cols);
+    gettimeofday(&stop, NULL);
+
+    // this is in seconds
+    double time_taken = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+
+    fprintf(fp, "\n%f, %d", time_taken * 1000  , matrix_size);
+    fclose(fp);
+    free(result);
+    free(a);
+    free(b);
+}
+
+int main()
+{
+
+    for (int i = 5; i < 500; i += 5)
+    {
+        printf("tested matrix size %d\n", i);
+        generate_data(i, "mmult_simd.out");
+    }
+}
