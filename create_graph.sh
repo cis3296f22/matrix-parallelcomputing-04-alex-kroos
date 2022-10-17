d4623904a534e4de15e7d8b2b8f849450bc7f4bf
mailoalex <tun29878@temple.edu>
2022-10-17 16:10:58 -0400

    updated create_graph.sh

--- create_graph.sh
+++ create_graph.sh
@@ -15,12 +15,12 @@ echo "Finished simd"
 
 # no O3
 echo "Testing  no O3"
-gcc mat.c mmult.c generate_data.c -o generate_data && ./generate_data "data/noO3.out" 
+gcc mat.c mmult_simd.c generate_data.c -o generate_data && ./generate_data "data/noO3.out" 
 echo "Finished no O3"
 
 #  O3
 echo "Testing O3"
-gcc -O3 mat.c mmult.c generate_data.c -o generate_data && ./generate_data "data/O3.out" 
+gcc -O3 mat.c mmult_simd.c  generate_data.c -o generate_data && ./generate_data "data/O3.out" 
 echo "Finished O3"
 
 
