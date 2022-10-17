8b853abdba8e8d17be556846cc3c62ce5a0f5b93
kroos <Kroosvi@gmail.com>
2022-10-17 12:28:02 -0400

    done with MPI and omp

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -1,4 +1,4 @@
-/** 
+/**
  * Incomplete program to multiply a matrix times a matrix using both
  * MPI to distribute the computation among nodes and OMP
  * to distribute the computation among threads.
@@ -25,6 +25,9 @@ int main(int argc, char* argv[])
     MPI_Status status;
 
     /* insert other global variables here */
+    int i, j, numsent, sender;
+    int anstype, row;
+    srand(time(0));
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
@@ -32,25 +35,74 @@ int main(int argc, char* argv[])
     if (argc > 1) {
         nrows = atoi(argv[1]);
         ncols = nrows;
-        
-        if (myid == 0) {
+
+        if (myid == 0) 
+        {
             // Controller Code goes here
             aa = gen_matrix(nrows, ncols);
             bb = gen_matrix(ncols, nrows);
-            cc1 = malloc(sizeof(double) * nrows * nrows); 
+            cc1 = malloc(sizeof(double) * nrows * nrows);
             starttime = MPI_Wtime();
             /* Insert your controller code here to store the product into cc1 */
+            numsent = 0;
+            MPI_Bcast(b, ncols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+            for (i = 0; i < min(numprocs-1, nrows); i++) {
+                for (j = 0; j < ncols; j++) {
+                  buffer[j] = aa[i * ncols + j];
+            }
+            MPI_Send(buffer, ncols, MPI_DOUBLE, i+1, i+1,MPI_COMM_WORLD);
+            numsent++;
+            }
+
+            for (i = 0; i < nrows; i++)
+            {
+                MPI_Recv(&ans, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                sender = status.MPI_SOURCE;
+                anstype = status.MPI_TAG;
+                c[anstype-1] = ans;
+
+                if (numsent < nrows) {
+                  for (j = 0; j < ncols; j++) 
+                    buffer[j] = aa[numsent*ncols + j];
+                  
+                  MPI_Send(buffer, ncols, MPI_DOUBLE, sender, numsent+1,
+                           MPI_COMM_WORLD);
+                  numsent++;
+                }else {MPI_Send(MPI_BOTTOM, 0, MPI_DOUBLE, sender, 0,MPI_COMM_WORLD);}
+            }
+
             endtime = MPI_Wtime();
             printf("%f\n",(endtime - starttime));
             cc2  = malloc(sizeof(double) * nrows * nrows);
             mmult(cc2, aa, nrows, ncols, bb, ncols, nrows);
             compare_matrices(cc2, cc1, nrows, nrows);
-        } else {
-            // Worder code goes here
-        }
-    } else {
-        fprintf(stderr, "Usage matrix_times_vector <size>\n");
+        } 
+        else 
+        {
+        // Worder code goes here
+        I_Bcast(b, ncols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        if (myid <= nrows) {
+            while(1) 
+            {
+                MPI_Recv(buffer, ncols, MPI_DOUBLE, 0,
+                MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                
+                if (status.MPI_TAG == 0){break;}
+                
+                row = status.MPI_TAG;
+                ans = 0.0;
+                
+                #pragma omp shared(ans) for reduction(+:ans)
+              
+                for (j = 0; j < ncols; j++)
+                    ans += buffer[j] * b[j];
+                MPI_Send(&ans, 1, MPI_DOUBLE, 0, row,MPI_COMM_WORLD);
+            }
+        } 
     }
+    }else {fprintf(stderr, "Usage matrix_times_vector <size>\n");}
+
+
     MPI_Finalize();
     return 0;
 }
